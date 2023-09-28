991b95601bc189bfbbe41899d87617e6e0a6c19d
JikoJeans <miguelmung5@gmail.com>
2023-09-28 14:53:44 -0700

    init

---
+++ src/counter.py
@@ -0,0 +1,44 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# we need to import the file that contains the status codes
+from src import status
+
+COUNTERS = {}
+
+# We will use the app decorator and create a route called slash counters.
+# specify the variable in route <name>
+# let Flask know that the only methods that is allowed to called
+# on this function is "POST".
+@app.route('/counters/<name>', methods=['POST'])
+def create_counter(name):
+    """Create a counter"""
+    app.logger.info(f"Request to create counter: {name}")
+    global COUNTERS
+    #line added from example
+    if name in COUNTERS:
+        return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
+    COUNTERS[name] = 0
+    return {name: COUNTERS[name]}, status.HTTP_201_CREATED
+
+#create a route for method PUT
+@app.route('/counters/<name>', methods=['PUT'])
+def update_counter(name):
+    #increment the counter by 1
+    COUNTERS[name] = COUNTERS[name] + 1
+    #return the new counter and a 200_ok return code
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+#create a route for method GET
+@app.route('/counters/<name>', methods=['GET'])
+def get_counter(name):
+    #check to see if the name does not exist and return 404 if not
+    if name not in COUNTERS:
+        return {"Message":f"Counter {name} does not exists"}, status.HTTP_404_NOT_FOUND
+    #if the name is in the counters list then return a status code okay
+    return {name: COUNTERS[name]}, status.HTTP_200_OK
+
+
+
+
---
+++ tests/test_counter.py
@@ -0,0 +1,73 @@
+"""
+Test Cases for Counter Web Service
+
+Create a service that can keep a track of multiple counters
+- API must be RESTful - see the status.py file. Following these guidelines, you can make assumptions about
+how to call the web service and assert what it should return.
+- The endpoint should be called /counters
+- When creating a counter, you must specify the name in the path.
+- Duplicate names must return a conflict error code.
+- The service must be able to update a counter by name.
+- The service must be able to read the counter
+"""
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+class CounterTest(TestCase):
+    """Counter tests"""
+#refactored code from prof
+    def setUp(self):
+        self.client = app.test_client()
+        name = 'CounterName'
+
+#example 1
+    def test_create_a_counter(self):
+        """It should create a counter"""
+        client = app.test_client()
+        result = client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+#example 2
+    def test_duplicate_a_counter(self):
+        """It should return an error for duplicates"""
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+#now we will create out own test called test_update_a_counter(self)
+    def test_update_a_counter(self):
+        #result should test the update method, first create a counter
+        counter = app.test_client()
+        basename = '/counters/foobar'
+        result = counter.post(basename)
+        #now test to make sure it was actually created
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        #we already store a baseline for the counter
+        base = result.get_json()['foobar']
+        #basename = '/counters/foobar'
+        #update the counter
+        result = counter.put(basename)
+        #now first test that it updated and assert they arent equal
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        #to check the counter value is one more than the baseline we need to pull the new baseline and compare
+        count = result.get_json()['foobar']
+        self.assertEqual(base+1, count)
+
+#for the read test we will create a counter and test the read function
+    def test_read_a_counter(self):
+        #create a test counter
+        counter = app.test_client()
+        #try to get counter name but no counter is there
+        result = counter.get('/counters/supersecretsite')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        #now add the counter to the list of counters
+        counter.post('/counters/supersecretsite')
+        #now counter is made so test get success code
+        result = counter.get('/counters/supersecretsite')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
