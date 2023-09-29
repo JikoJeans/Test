cdb50292e7638f8c7cff9ebd1c4656a4609a0bfa
JikoJeans <miguelmung5@gmail.com>
2023-09-29 11:17:31 -0700

    again removed errors from flake8

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -20,19 +20,19 @@ from src import status
 
 class CounterTest(TestCase):
     """Counter tests"""
-#refactored code from prof
+# refactored code from prof
     def setUp(self):
         self.client = app.test_client()
         name = 'CounterName'
 
-#example 1
+# example 1
     def test_create_a_counter(self):
         """It should create a counter"""
         client = app.test_client()
         result = client.post('/counters/foo')
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
 
-#example 2
+# example 2
     def test_duplicate_a_counter(self):
         """It should return an error for duplicates"""
         result = self.client.post('/counters/bar')
@@ -40,34 +40,34 @@ class CounterTest(TestCase):
         result = self.client.post('/counters/bar')
         self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
 
-#now we will create out own test called test_update_a_counter(self)
+# now we will create out own test called test_update_a_counter(self)
     def test_update_a_counter(self):
-        #result should test the update method, first create a counter
+        # result should test the update method, first create a counter
         counter = app.test_client()
         basename = '/counters/foobar'
         result = counter.post(basename)
-        #now test to make sure it was actually created
+        # now test to make sure it was actually created
         self.assertEqual(result.status_code, status.HTTP_201_CREATED)
-        #we already store a baseline for the counter
+        # we already store a baseline for the counter
         base = result.get_json()['foobar']
-        #basename = '/counters/foobar'
-        #update the counter
+        # basename = '/counters/foobar'
+        # update the counter
         result = counter.put(basename)
-        #now first test that it updated and assert they arent equal
+        # now first test that it updated and assert they arent equal
         self.assertEqual(result.status_code, status.HTTP_200_OK)
-        #to check the counter value is one more than the baseline we need to pull the new baseline and compare
+        # to check the counter value is one more than the baseline we need to pull the new baseline and compare
         count = result.get_json()['foobar']
         self.assertEqual(base+1, count)
 
-#for the read test we will create a counter and test the read function
+# for the read test we will create a counter and test the read function
     def test_read_a_counter(self):
-        #create a test counter
+        # create a test counter
         counter = app.test_client()
-        #try to get counter name but no counter is there
+        # try to get counter name but no counter is there
         result = counter.get('/counters/supersecretsite')
         self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
-        #now add the counter to the list of counters
+        # now add the counter to the list of counters
         counter.post('/counters/supersecretsite')
-        #now counter is made so test get success code
+        # now counter is made so test get success code
         result = counter.get('/counters/supersecretsite')
         self.assertEqual(result.status_code, status.HTTP_200_OK)
